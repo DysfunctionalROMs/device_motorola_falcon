560848afd69a59a2bc8dec7474b1c38ec42c0c41
robt77 <robthompson1977@gmail.com>
2014-10-31 22:49:42 +0000

    update broken.mk for OTA proprties

--- broken.mk
+++ broken.mk
@@ -1,7 +1,11 @@
+# Broken
+
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
+# Release name
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := broken_falcon
+PRODUCT_DEVICE := falcon
