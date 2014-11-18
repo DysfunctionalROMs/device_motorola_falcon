14051f2456ec6046fc33afec6e58acf510421dca
robt77 <robthompson1977@gmail.com>
2014-11-18 00:09:19 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -31,7 +31,3 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from msm8226-common
 $(call inherit-product, device/motorola/msm8226-common/msm8226.mk)
-
-
-# Build with Dalvik Runtime as default
-PRODUCT_RUNTIMES := runtime_libdvm_default 
