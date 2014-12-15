89bfd243c95d2daecfba8e0af09c60118c6ad385
robt77 <robthompson1977@gmail.com>
2014-12-15 23:12:15 +0000

    Update broken.mk

--- broken.mk
+++ broken.mk
@@ -3,5 +3,8 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 # Inherit some common Broken stuff.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_DEVICE := falcon
 PRODUCT_NAME := broken_falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g
+PRODUCT_MANUFACTURER := motorola
