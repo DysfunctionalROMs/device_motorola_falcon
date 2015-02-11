ccdfa8b8798ed83b4ce8cdf81c1403c4ecc76798
robt77 <robthompson1977@gmail.com>
2015-02-11 17:39:02 +0000

    update tool chain again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,9 +24,8 @@ TARGET_OTA_ASSERT_DEVICE := xt1031,xt1032,xt1033,xt1034,falcon_umts,falcon_umtsd
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := falcon_defconfig
-TARGET_GCC_VERSION_ARM := 4.10-sm
-TARGET_GCC_VERSION_AND := 4.10-sm
-
+TARGET_GCC_VERSION_ARM := 4.9-sm-robt77
+TARGET_GCC_VERSION_EXP := 4.9-sm-robt77
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_falcon.c
