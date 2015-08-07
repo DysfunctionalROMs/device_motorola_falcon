CyanogenMod/android_device_motorola_falcon

falcon: Add device specific hooks to ramdisk
Change-Id: I53c81ccb4906081a58f99d49fbfc49476472f0fb
Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

device.mk
@@ -26,7 +26,8 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.device.rc
 
 # Wifi
 PRODUCT_COPY_FILES += \

DysfunctionalROMs/device_motorola_falcon

device.mk
@@ -30,7 +30,8 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.target.rc
 
 # Wifi
 PRODUCT_COPY_FILES += \