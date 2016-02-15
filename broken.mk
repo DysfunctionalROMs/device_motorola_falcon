49c79c06596bab019a5719b6fa75a7207a0ef2c9
robt77 <robthompson1977@gmail.com>
2016-02-15 21:20:37 +0000

    broke it

---
+++ broken.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+PRODUCT_DEVICE := falcon
+PRODUCT_NAME := broken_falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
