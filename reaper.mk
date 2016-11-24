c25b062145d2cc09093194110f65065f366411f9
robt77 <robthompson1977@gmail.com>
2016-11-24 22:44:00 +0000

    Put the scythe to it

--- reaper.mk
+++ reaper.mk
@@ -5,9 +5,9 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Inherit some common Broken stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+$(call inherit-product, vendor/reaper/config/common_full_phone.mk)
 PRODUCT_DEVICE := falcon
-PRODUCT_NAME := broken_falcon
+PRODUCT_NAME := reaper_falcon
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto G
 PRODUCT_MANUFACTURER := motorola
@@ -15,6 +15,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=falcon PRODUCT_NAME=falcon
 TARGET_VENDOR := Motorola
 
-# Broken Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Rob Thompson (robt77)"
